15cff22ef09f329c9bf4aaa114587aae54aa5f42
sskkyy0 <zxzxzxzxzxzxzz@naver.com>
2025-02-05 00:45:13 +0900

    로또 생성

--- src/main/java/org/example/haneul/lotto/Application.java
+++ src/main/java/org/example/haneul/lotto/Application.java
@@ -2,7 +2,8 @@ package org.example.haneul.lotto;
 
 public class Application {
     public static void main(String[] args) {
-//        LottoGameController controller = new LottoGameController();
-//        controller.play();
+        LottoGameController controller = new LottoGameController();
+        controller.play();
     }
 }
+
--- src/main/java/org/example/haneul/lotto/LottoGameController.java
+++ src/main/java/org/example/haneul/lotto/LottoGameController.java
@@ -10,12 +10,15 @@ public class LottoGameController {
     Lotto lotto = new Lotto();
     List<List> numbers = new ArrayList<>(); // 구매한 로또 리스트
     List<Integer> win=new ArrayList<>(); // 당첨번호 리스트
-    int amount; //구입 금액
+    int amount, bonus; //구매 개수와 보너스 번호
+    int[] result=new int[5];
 
     public void play() {
         printer.print("구입금액을 입력해 주세요.");
-        amount = scanner.nextInt()/1000;
-        if (amount % 1000 != 0){
+        int price = scanner.nextInt();
+        amount = price/1000;
+
+        if (price % 1000 != 0){
             printer.printError();
             return;
         }
@@ -24,6 +27,21 @@ public class LottoGameController {
 
         printer.print("당첨번호를 입력해주세요.");
         String line=scanner.nextLine();
+        String[] number = line.split(",");
+        for (String s : number) {
+            win.add(Integer.parseInt(s));
+        }
+
+        printer.print("보너스 번호를 입력해주세요.");
+        bonus = scanner.nextInt();
+        if (win.contains(bonus)) {
+            printer.printError();
+            return;
+        }
+
+        check();
+
+        printer.print("당첨 통계\n---\n");
 
     }
 
@@ -31,12 +49,28 @@ public class LottoGameController {
         while (numbers.size() < amount) {
             numbers.add(lotto.create());
         }
+
         for (List list : numbers) {
             printer.printLotto(list);
         }
     }
 
     private void check(){
+        for (List number : numbers) {
+            checkLotto(number);
+        }
+    }
+
+    private void checkLotto(List<Integer> list) {
+        int count=0;
+        boolean b=false;
+        for (int i:list){
+            if (win.contains(i)){
+                count++;
+            }
+        }
+        if (list.contains(bonus))
+            b=true;
 
     }
 
