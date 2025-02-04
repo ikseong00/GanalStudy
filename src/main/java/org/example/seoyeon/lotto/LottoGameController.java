6fad6619bd9ed54044229214f3e625817f48c8d5
yeonzeop <ksp2617@naver.com>
2025-02-04 21:37:15 +0900

    코드 수정

--- src/main/java/org/example/seoyeon/lotto/LottoGameController.java
+++ src/main/java/org/example/seoyeon/lotto/LottoGameController.java
@@ -66,36 +66,35 @@ public class LottoGameController {
 
     private String enterMoney(String str) {
         String moneyStr = str.replaceAll("[ ,]|won|원", "");
-        moneyStr = moneyStr.replace("백", "00");
         if (moneyStr.indexOf("십") == 0) {
-            moneyStr=moneyStr.replace("십", "10");
+            moneyStr = moneyStr.replace("십", "10");
         } else {
             moneyStr = moneyStr.replace("십", "0");
         }
         if (moneyStr.indexOf("백") == 0) {
-            moneyStr=moneyStr.replace("백", "100");
+            moneyStr = moneyStr.replace("백", "100");
         } else {
             moneyStr = moneyStr.replace("백", "00");
         }
         if (moneyStr.indexOf("천") == 0) {
-            moneyStr=moneyStr.replace("천", "1000");
+            moneyStr = moneyStr.replace("천", "1000");
         } else {
             moneyStr = moneyStr.replace("천", "000");
         }
         if (moneyStr.indexOf("만") == 0) {
-            moneyStr=moneyStr.replace("만", "10000");
+            moneyStr = moneyStr.replace("만", "10000");
         } else {
             moneyStr = moneyStr.replace("만", "0000");
         }
-        moneyStr=moneyStr.replace("일","1");
-        moneyStr=moneyStr.replace("이","2");
-        moneyStr=moneyStr.replace("삼","3");
-        moneyStr=moneyStr.replace("사","4");
-        moneyStr=moneyStr.replace("오","5");
-        moneyStr=moneyStr.replace("육","6");
-        moneyStr=moneyStr.replace("칠","7");
-        moneyStr=moneyStr.replace("팔","8");
-        moneyStr=moneyStr.replace("구","9");
+        moneyStr = moneyStr.replace("일", "1");
+        moneyStr = moneyStr.replace("이", "2");
+        moneyStr = moneyStr.replace("삼", "3");
+        moneyStr = moneyStr.replace("사", "4");
+        moneyStr = moneyStr.replace("오", "5");
+        moneyStr = moneyStr.replace("육", "6");
+        moneyStr = moneyStr.replace("칠", "7");
+        moneyStr = moneyStr.replace("팔", "8");
+        moneyStr = moneyStr.replace("구", "9");
 
         return moneyStr;
     }
