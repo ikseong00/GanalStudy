0f445c21d3a244243bba7b3906b251258c9b0593
choihongjun <vvclsbalam@gmail.com>
2025-02-12 02:22:53 +0900

    feat: commit test

--- src/main/java/org/example/hongjun/test/Test.java
+++ src/main/java/org/example/hongjun/test/Test.java
@@ -2,6 +2,6 @@ package org.example.hongjun.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("test");
     }
 }
